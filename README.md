853e81512fc582fa22cbbe5680c307499bc9f12d
Pavel_Evsaev <pavel.evsaev@gmail.com>
2016-11-17 15:49:05 +0300

    README is updated. Shell Script file is added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # macOS-App-Uninstaller
 Script to delete an app and related files from your mac
+
+1. clone repo from github
+2. open macOS-App-Uninstaller folder
+3. give execute permissions to delete_app.sh file
+4. run delete_app.sh with application name param
+
+Example:
+
+git clone https://github.com/PavelEvsaev/macOS-App-Uninstaller.git
+cd ./macOS-App-Uninstaller/
+chmod u+x delete_app.sh
+./delete_app.sh APP_TO_DELETE_NAME
---
+++ delete_app.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+ask_to_delete() {
+	echo "Delete file(y/n)?"
+	echo "$1"
+}
+
+need_to_delete() {
+
+	ask_to_delete $1
+
+	read -p "Input (y/n):" answer < /dev/tty
+
+	if echo "$answer" | grep "^y"; then
+		return 0
+	else 
+		return 1
+	fi
+}
+
+is_empty() {
+	if [ -s "$1" ]; then
+		return 1
+	else
+	    return 0
+	fi
+}
+
+delete() {
+	rm -rf "$1"
+}
+
+related_files="related_files.txt"
+mdfind -name $1 > $related_files
+
+if is_empty $related_files; then
+	echo "No '$1' application files on your computer" 
+	delete $related_files
+    exit 0
+fi
+
+cat $related_files | while read filename; do
+	if need_to_delete $filename; then
+		delete $filename
+	fi
+done
+
+delete $related_files
+exit 0
